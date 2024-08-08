6841257a7b8b87fc49d8c11077c928e1e1c68eac
Spectre <olegspectre@yandex.ru>
2024-08-08 16:30:03 +0300

    Create infex.php

---
+++ lang/ru/install/infex.php
@@ -0,0 +1,10 @@
+<?
+$MESS["linkslite_install_name"] = "Модуль перелинковки Lite версия";
+$MESS["linkslite_install_descr"] = "Модуль перелинковки или быстрые ссылки версия lite, работает с Highload блоками";
+$MESS["linkslite_install_partner"] = "Дизайн-студия Spectre";
+$MESS["linkslite_install_url"] = "https://spectre.su/";
+$MESS["linkslite_install_install"] = "Установка модуля 'Перелинковка LITE'";
+$MESS["linkslite_install_unistall"] = "Деинсталляция модуля 'Перелинковка LITE'";
+$MESS["linkslite_install_save_files"] = "Сохранить файлы компонентов?";
+$MESS["linkslite_install_save_hl"] = "Сохранить таблицу?";
+?>
