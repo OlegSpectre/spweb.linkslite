7acee949e535822aea930a200b3010a9db3434f6
Spectre <olegspectre@yandex.ru>
2024-08-08 16:29:14 +0300

    Create index.php

---
+++ lang/en/install/index.php
@@ -0,0 +1,10 @@
+<?
+$MESS["linkslite_install_name"] = "Linking Module Lite version";
+$MESS["linkslite_install_descr"] = "The linking module or sitelinks lite version, works with Highload blocks";
+$MESS["linkslite_install_partner"] = "Spectre Design Studio";
+$MESS["linkslite_install_url"] = "https://spectre.su/";
+$MESS["linkslite_install_install"] = "Installing the module 'Relink LITE'";
+$MESS["linkslite_install_unistall"] = "Uninstalling the module 'Relink LITE'";
+$MESS["linkslite_install_save_files"] = "Save component files?";
+$MESS["linkslite_install_save_hl"] = "Save table?";
+?>
